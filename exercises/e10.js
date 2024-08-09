8e9ad2c9267e3a4fd4464f1f74111cde5c6bc976
damonryon <damon.ryon@gmail.com>
2024-08-09 11:06:27 -0700

    solve e10

--- exercises/e10.js
+++ exercises/e10.js
@@ -1,12 +1,45 @@
+// const bankAccounts = [
+//   {
+//     id: 1,
+//     name: "Susan",
+//     balance: 100.32,
+//     deposits: [150, 30, 221],
+//     withdrawals: [110, 70.68, 120],
+//   },
+//   { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+//   {
+//     id: 3,
+//     name: "Joshua",
+//     balance: 18456.57,
+//     deposits: [4000, 5000, 6000, 9200, 256.57],
+//     withdrawals: [1500, 1400, 1500, 1500],
+//   },
+//   { id: 4, name: "Candy", balance: 0.0 },
+//   { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+// ];
+
+
 // EXERCISE 10
 // Return an array of account holders names that contains a given letter
 // Array example: bankAccounts in /data/data.js
 // getClientsWithLetterInName(bankAccounts, 'e') => ['Kevin', 'Steven', ...]
 
-export function getClientsWithLetterInName(array, letter) {
-  // Your code goes here...
-
+ export function getClientsWithLetterInName(array, letter) {
+  let arr = []
+  for(let i = 0; i < array.length; i++) {
+    let el = array[i]
+    let name = el.name.toLowerCase()
+    if(name.includes(letter)){
+      arr.push(el.name)
+    }
+  }
+  console.log(arr)
+  return arr
 }
+
+
+// getClientsWithLetterInName(bankAccounts, 'a')
+// getClientsWithLetterInName(bankAccounts, 'c')
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-10"
