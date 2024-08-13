b795309e715d9b207e01efa6a9d683082d3cd090
damonryon <damon.ryon@gmail.com>
2024-08-13 12:11:54 -0700

    refactor 20

--- exercises/e20.js
+++ exercises/e20.js
@@ -1,21 +1,3 @@
-// const classSeatsFlattened = [
-//   "Ben",
-//   "Emma",
-//   "Sophia",
-//   "William",
-//   "Elijah",
-//   "James",
-//   "Lucas",
-//   "Mason",
-//   "Mia",
-//   "Liam",
-//   "Noah",
-//   "Oliver",
-//   "Ethan",
-//   "Harper",
-//   "Evelyn",
-// ];
-
 
 // EXERCISE 20
 // Return and array of 2 arrays
@@ -23,22 +5,22 @@
 // separateNamesWithAFromRest(["jon", "alice", "andrew", "mark", "jimmy"]) -> [["alice", "andrew", "mark"], ["jon", "jimmy"]]
 // NOTE: You MUST use double/nested FOR loop to solve this exercise. The array.includes() method is NOT allowed.
 
+
+
 export function separateNamesWithAFromRest(array) {
   let arr1 = []
   let arr2 = []
-  for(let i = 0; i < array.length; i++){
-    let el = array[i]
-    let sum = 0
-    for(let j = 0; j < el.length; j++){
-      let ele = el[j]
-      if(ele === 'a'){
-        sum++
+  for (let name of array) {
+    let hasA = false
+    for (let i = 0; i < name.length; i++) {
+      if (name[i] === 'a' || name[i] === 'A') {
+        hasA = true
       }
     }
-    if(sum > 0){
-      arr1.push(el)
+    if (hasA) {
+      arr1.push(name)
     } else {
-      arr2.push(el)
+      arr2.push(name)
     }
   }
   return [arr1, arr2]
